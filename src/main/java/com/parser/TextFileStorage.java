054aa04d07b4abe65fb479a75dbe035a8e85b4a3
Konstantin Krinitskiy <kkrinitskiy@internet.ru>
2024-08-10 16:55:22 +0300

    add code to remove old outputs, before creating new

--- src/main/java/com/parser/TextFileStorage.java
+++ src/main/java/com/parser/TextFileStorage.java
@@ -6,13 +6,20 @@ import com.parser.dto.Cryptocurrency;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class TextFileStorage {
 
     public void store(List<Cryptocurrency> cryptocurrencyList, String siteName){
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("output/" + siteName + "_output.txt"))) {
-                for (Cryptocurrency cryptocurrency : cryptocurrencyList) {
+        try{
+            Files.deleteIfExists(Path.of("output/" + siteName + "_output.txt"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output/" + siteName + "_output.txt", false))) {
+            for (Cryptocurrency cryptocurrency : cryptocurrencyList) {
                     writer.write(cryptocurrency.name() + " - " + cryptocurrency.currentRate() + "\n");
                 }
             } catch (IOException e) {
